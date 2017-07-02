2080e0c0821928bb4dd46356f16b139b755f3c29
Viktor Adam <rycus86@gmail.com>
2017-07-02 21:08:12 +0100

    Fixing public Docker image name

--- travis-cli.sh
+++ travis-cli.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -it -v $PWD:/travis -v ~/.travis-cli:/home/travis-cli travis-cli $@
+docker run --rm -it -v $PWD:/travis -v ~/.travis-cli:/home/travis-cli rycus86/travis-cli $@
